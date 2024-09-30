0bd2ff41b4e8baa8f11b01cf41a0d8b9d834b3ed
Dabi1290 <davide.amoruso33@gmail.com>
2024-09-30 10:14:59 +0200

    hello_world

---
+++ helloworld/target/debug/deps/helloworld-30ec20eb1bc7abe7.d
@@ -0,0 +1,5 @@
+/Users/davideamoruso/Documents/projects/rust/helloworld/target/debug/deps/helloworld-30ec20eb1bc7abe7: src/main.rs
+
+/Users/davideamoruso/Documents/projects/rust/helloworld/target/debug/deps/helloworld-30ec20eb1bc7abe7.d: src/main.rs
+
+src/main.rs:
---
+++ helloworld/target/debug/helloworld.d
@@ -0,0 +1,1 @@
+/Users/davideamoruso/Documents/projects/rust/helloworld/target/debug/helloworld: /Users/davideamoruso/Documents/projects/rust/helloworld/src/main.rs
